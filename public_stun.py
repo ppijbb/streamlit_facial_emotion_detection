2efd9deeb38dae937b5952408d805995e7a5d3e3
Kevin.Jung <kevin.jung@turingbio.com>
2023-01-26 18:12:32 +0900

    stun 서버 순서 변경 절대경로 수정

--- public_stun.py
+++ public_stun.py
@@ -1,6 +1,6 @@
 public_stun_server_list = [
-    "stun:openrelay.metered.ca:80",
     "stun:stun.xten.com:3478",
+    "stun:openrelay.metered.ca:80",
     "stun:stun.stunprotocol.org:3478",
     "stun:stun.l.google.com:19302",
     "stun:stun1.l.google.com:19302",
