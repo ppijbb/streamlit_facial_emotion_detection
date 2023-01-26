a2f2c25b5affaba708dfdcc7ceb861a109a51bbc
Kevin.Jung <kevin.jung@turingbio.com>
2023-01-26 13:26:06 +0900

    각 클래스별 confidence 추가

--- rtc_page.py
+++ rtc_page.py
@@ -5,17 +5,18 @@ import numpy as np
 import av
 import json
 from typing import List
-import mediapipe as mp
+
 from datetime import datetime as dt
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 from streamlit_webrtc.models import VideoProcessorBase
-
+from PIL import ImageFont, ImageDraw, Image
 import tensorflow as tf
 from tensorflow.compat.v1 import ConfigProto, InteractiveSession
 from tensorflow.keras.models import model_from_json
 from tensorflow.keras.preprocessing import image as _IMG
-from streamlit.components.v1 import html
+# from streamlit.components.v1 import html
+# import mediapipe as mp
 
 config = ConfigProto()
 config.gpu_options.allow_growth = True
@@ -35,89 +36,145 @@ model = model_from_json(open("caer_face.json", "r").read())
 # load weights
 model.load_weights('caer_face.h5')
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5
-)
+# mp_drawing = mp.solutions.drawing_utils
+# mp_drawing_styles = mp.solutions.drawing_styles
+# mp_hands = mp.solutions.hands
+# hands = mp_hands.Hands(
+#     model_complexity=0,
+#     min_detection_confidence=0.5,
+#     min_tracking_confidence=0.5
+# )
 
 # face detection
-face_haar_cascade = cv2.CascadeClassifier("/home/ubuntu/.local/lib/python3.9/site-packages/cv2/data/haarcascade_frontalface_default.xml")
-# face_haar_cascade = cv2.CascadeClassifier("C:\\Users\\Lenovo\\.conda\\envs\\python_3_9_env\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml")
+# face_haar_cascade = cv2.CascadeClassifier("/home/ubuntu/.local/lib/python3.9/site-packages/cv2/data/haarcascade_frontalface_default.xml")
+face_haar_cascade = cv2.CascadeClassifier("C:\\Users\\Lenovo\\.conda\\envs\\python_3_9_env\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml")
 
 
 def process_face(image):
+    font_path = "font/jalnan/yg-jalnan.ttf"
+    font_regular = ImageFont.truetype(font=font_path, size=20)
+    font_small = ImageFont.truetype(font=font_path, size=10)
+    paint_width = image.shape[1]
+    emotions = ['happy', 'sad', 'neutral']
+    labels = ["긍정", "부정", "중립"]
+    labels_y = [10, 25, 40]
+    labels_c = [(0, 255, 0), (0, 0, 255), (125, 125, 125)]
+    start_x = int(30)
+    end_x = int(paint_width / 3)
+
     try:
         image.flags.writeable = False
         gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
         faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
         result = None
-        paint_width = gray_img.shape[1]
         cv2.rectangle(image, (0, 0), (paint_width, 50), (255, 255, 255), thickness=-1)
+        cv2.rectangle(image, (0, 0), (start_x + end_x + 15, 50), (196, 196, 196), thickness=-1)
+        if 0 < len(faces_detected):
+            for (x, y, w, h) in faces_detected:
+                # print('WORKING')
+                # print(gray_img.shape)
 
-        for (x, y, w, h) in faces_detected:
-            # print('WORKING')
-            # print(gray_img.shape)
+                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 0), thickness=5)
+                roi_gray = gray_img[y - 10:y + w + 10,
+                                    x - 10:x + h + 10]  # cropping region of interest i.e. face area from  image
+                roi_gray = cv2.resize(roi_gray, (224, 224))
+                img_pixels = _IMG.img_to_array(roi_gray)
+                img_pixels = np.expand_dims(img_pixels, axis=0)
+                img_pixels *= 0.8
 
-            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
-            roi_gray = gray_img[y - 10:y + w + 10,
-                       x - 10:x + h + 10]  # cropping region of interest i.e. face area from  image
-            roi_gray = cv2.resize(roi_gray, (224, 224))
-            img_pixels = _IMG.img_to_array(roi_gray)
-            img_pixels = np.expand_dims(img_pixels, axis=0)
-            img_pixels *= 0.8
+                # print(img_pixels.shape)
 
-            # print(img_pixels.shape)
+                predictions = model.predict(img_pixels)
+                # find max indexed array
 
-            predictions = model.predict(img_pixels)
-            # find max indexed array
+                max_index = np.argmax(predictions[0])
 
-            max_index = np.argmax(predictions[0])
+                # emotions = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 
-            # emotions = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
-            emotions = ['happy', 'sad', 'neutral']
-            predicted_emotion = emotions[max_index]
-            result = {
-                dt.now().strftime("%Y-%m-%dT%H:%M:%S"):
-                   {
-                      "result": predicted_emotion,
-                      "happy": predictions[0][0],
-                      "sad": predictions[0][1],
-                      "neutral": predictions[0][2]
+                predicted_emotion = emotions[max_index]
+                result = {
+                    dt.now().strftime("%Y-%m-%dT%H:%M:%S"):
+                       {
+                          "result": predicted_emotion,
+                          "happy": predictions[0][0],
+                          "sad": predictions[0][1],
+                          "neutral": predictions[0][2]
                     }
-            }
-            cv2.putText(image, predicted_emotion if predicted_emotion else "No Face", (int(paint_width/3), int(25)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-            cv2.putText(image, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                }
+                for i, (h, c) in enumerate(zip(labels_y, labels_c)):
+                    cv2.line(image, (start_x, h), (int(start_x + end_x * predictions[0][i]), h), c, 10)  # 클래스별 confidence
 
-        return image, result
+                image_pil = Image.fromarray(image)
+                draw = ImageDraw.Draw(image_pil)
+                for index, h in zip(labels, labels_y):
+                    draw.text(xy=(int(3), int(h-5)),
+                              text=index,
+                              font=font_small,
+                              fill=(0, 0, 0, 0))
+                draw.text(xy=(int(paint_width / 1.5), int(5)),
+                          text=labels[max_index],
+                          font=font_regular,
+                          fill=(0, 0, 0, 0))
+                draw.text(xy=(int(x+10), int(y-30)),
+                          text=labels[max_index],
+                          font=font_regular,
+                          fill=(255, 0, 0, 0))
+                processed = np.array(image_pil)
+        else:
+            for h, c in zip(labels_y, labels_c):
+                cv2.line(image, (start_x, h), (int(start_x + end_x * 0.0005), h), c, 10)  # 클래스별 confidence
+
+            image_pil = Image.fromarray(image)
+            draw = ImageDraw.Draw(image_pil)
+            for l, h in zip(labels, labels_y):
+                draw.text(xy=(int(3), int(h-5)),
+                          text=l,
+                          font=font_small,
+                          fill=(0, 0, 0, 0))
+            draw.text(xy=(int(paint_width / 1.5), int(5)),
+                      text="얼굴이 인식되지 않았습니다.",
+                      font=font_regular,
+                      fill=(0, 0, 0, 0))
+            processed = np.array(image_pil)
+        return processed, result
 
     except Exception as e:
         print("Exception", e)
-        return cv2.flip(image, 1), None
+        for h, c in zip(labels_y, labels_c):
+            cv2.line(image, (start_x, h), (int(start_x + end_x * 0.0005), h), c, 10)  # 클래스별 confidence
+        image_pil = Image.fromarray(image)
+        draw = ImageDraw.Draw(image_pil)
+        for index, h in zip(labels, labels_y):
+            draw.text(xy=(int(3), int(h-5)),
+                      text=index,
+                      font=font_small,
+                      fill=(0, 0, 0, 0))
+        draw.text(xy=(int(paint_width / 1.5), int(5)),
+                  text="얼굴이 인식되지 않았습니다.",
+                  font=font_regular,
+                  fill=(0, 0, 0, 0))
+        processed = np.array(image_pil)
+        return processed, None
 
 
-def process(image):
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
-
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-    return image
-    # return cv2.flip(image, 1)
+# def process(image):
+#     image.flags.writeable = False
+#     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#     results = hands.process(image)
+#
+#     # Draw the hand annotations on the image.
+#     image.flags.writeable = True
+#     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+#     if results.multi_hand_landmarks:
+#       for hand_landmarks in results.multi_hand_landmarks:
+#         mp_drawing.draw_landmarks(
+#             image,
+#             hand_landmarks,
+#             mp_hands.HAND_CONNECTIONS,
+#             mp_drawing_styles.get_default_hand_landmarks_style(),
+#             mp_drawing_styles.get_default_hand_connections_style())
+#     return image
+#     # return cv2.flip(image, 1)
 
 
 RTC_CONFIGURATION = RTCConfiguration(
@@ -159,6 +216,7 @@ class VideoProcessor(VideoProcessorBase):
             print("error")
 
 
+# @st.cache(suppress_st_warning=True)
 def show():
     queries = st.experimental_get_query_params()
     code = queries.get("code", None)[0]
@@ -184,4 +242,9 @@ def show():
 
 
 if __name__ == "__main__":
+    st.set_page_config(
+        page_title="CAERScope",
+        layout="wide",
+        initial_sidebar_state="expanded",
+    )
     show()
