e37e12c44175520dd14ba064e210223625ce3630
Kevin.Jung <kevin.jung@turingbio.com>
2023-01-30 15:46:35 +0900

    stun, turn 서버 목록 최신화

--- rtc_page.py
+++ rtc_page.py
@@ -199,6 +199,21 @@ RTC_CONFIGURATION = RTCConfiguration(
             "urls": "turn:openrelay.metered.ca:443?transport=tcp",
             "username": "openrelayproject",
             "credential": "openrelayproject",
+        },
+        {
+            "urls": "turn:numb.viagenie.ca",
+            "username": 'webrtc@live.com',
+            "credential": 'muazkh'
+        },
+        {
+            "urls": "turn:192.158.29.39:3478?transport=udp",
+            "username": '28224511:1379330808',
+            "credential": 'JZEOEt2V3Qb0y27GRntt2u2PAYA='
+        },
+        {
+            "urls": "turn:192.158.29.39:3478?transport=tcp",
+            "username": '28224511:1379330808',
+            "credential": 'JZEOEt2V3Qb0y27GRntt2u2PAYA='
         }]
     })
 
