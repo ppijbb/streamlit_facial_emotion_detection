2e8b87f4d4208b2c04189a73f2bea43ff6960d25
Kevin.Jung <kevin.jung@turingbio.com>
2023-06-23 10:04:35 +0900

    facial emotion detection add

---
+++ src/orchestrator.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from . import examples
+from .. import rtc_page
+
+
+def show_examples():
+    st.write(
+        """
+        # 📸 st.camera_input
+
+        We are launching a new widget, which lets the user take an image through their 
+        webcam and upload it to the app! Use it with:
+
+        ```python
+        img_file = st.camera_input("Take a picture")
+        ```
+
+        More info in [our docs](https://docs.streamlit.io/library/api-reference/widgets/st.camera_input).
+
+        ---
+
+        And here's a demo that lets you apply a filter to the uploaded image:
+        """
+    )
+
+    examples.show()
+
+
+def show_rtc():
+    st.write(
+        """
+        # 📸 st.camera_input
+
+        We are launching a new widget, which lets the user take an image through their 
+        webcam and upload it to the app! Use it with:
+
+        ```python
+        img_file = st.camera_input("Take a picture")
+        ```
+
+        More info in [our docs](https://docs.streamlit.io/library/api-reference/widgets/st.camera_input).
+
+        ---
+
+        And here's a demo that lets you apply a filter to the uploaded image:
+        """
+    )
+
+    rtc_page.show()
+
+if __name__ == "__main__":
+    pass
